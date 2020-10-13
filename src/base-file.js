53d0f73b2a7b32d9a1302df4b2dbf85ab1966fc8
Praveen Madhav <praveen@epiphani.io>
2020-10-13 10:24:32 -0700

    Handle multiple buttons on a table row. Actions on files need a file object now instead of a string. Added support for view & refresh buttons. Made changes such that student files cannot be drop targets.

--- src/base-file.js
+++ src/base-file.js
@@ -72,8 +72,32 @@ class BaseFile extends React.Component {
     })
   }
   handleItemClick = (event) => {
+    console.log("handleItemClick:", this.props, event.target);
+    if (event.target) {
+      console.log("handleItemClick:", event.target.tagName);
+      if (typeof event.target.tagName === "string") {
+        // Font-Awesome button clicked, return to handle it
+        return;
+        /*if (event.target.tagName === "I") {
+          if (event.target.tagName.indexOf("fa-history")) {
+            console.log("handleItemClick: Processing refresh action");
+            //this.handleRefreshSubmit(event);
+            return;
+          } else if (event.target.tagName.indexOf("fa-eye")) {
+            console.log("handleItemClick: Processing view action");
+            //this.handleViewSubmit(event);
+            return;
+          }
+        }*/
+      }
+    }
+    if (event.target && event.target.type === "submit") {
+      // Handle delete confirm button
+      console.log("handleItemClick: Processing target action");
+      return;
+    }
     event.stopPropagation()
-    this.props.browserProps.select(this.props.fileKey, 'file', event.ctrlKey || event.metaKey, event.shiftKey)
+    this.props.browserProps.select(this.props, 'file', event.ctrlKey || event.metaKey, event.shiftKey)
   }
   handleItemDoubleClick = (event) => {
     event.stopPropagation()
@@ -84,17 +108,20 @@ class BaseFile extends React.Component {
     if (!this.props.browserProps.renameFile) {
       return
     }
-    this.props.browserProps.beginAction('rename', this.props.fileKey)
+    console.log("handleRenameClick: BaseFile:", this.props);
+    this.props.browserProps.beginAction('rename', this.props)
   }
   handleNewNameChange = (event) => {
     const newName = event.target.value
     this.setState({ newName: newName })
   }
   handleRenameSubmit = (event) => {
+    console.log("handleRenameSubmit entered")
     if (event) {
       event.preventDefault()
     }
     if (!this.props.browserProps.renameFile) {
+      console.log("handleRenameSubmit no renameFile")
       return
     }
     const newName = this.state.newName.trim()
@@ -105,6 +132,7 @@ class BaseFile extends React.Component {
       //   title: 'Invalid new file name',
       //   body: 'File name cannot be blank',
       // })
+      console.log("handleRenameSubmit empty new name")
       return
     }
     const invalidChar = ['/', '\\']
@@ -120,22 +148,48 @@ class BaseFile extends React.Component {
     if (slashIndex !== -1) {
       newKey = `${this.props.fileKey.substr(0, slashIndex)}/${newName}`
     }
-    this.props.browserProps.renameFile(this.props.fileKey, newKey)
+    console.log("handleRenameSubmit", this.props, newKey);
+    this.props.browserProps.renameFile(this.props, newKey)
   }
 
   handleDeleteClick = (event) => {
     if (!this.props.browserProps.deleteFile) {
       return
     }
-    this.props.browserProps.beginAction('delete', this.props.fileKey)
+    console.log("handleDeleteClick: BaseFile:", this.props);
+    this.props.browserProps.beginAction('delete', this.props)
   }
   handleDeleteSubmit = (event) => {
     event.preventDefault()
+    console.log("handleDeleteSubmit:", event, this.props);
     if (!this.props.browserProps.deleteFile) {
+      console.log("handleDeleteSubmit: deleteFile not set");
       return
     }
 
-    this.props.browserProps.deleteFile(this.props.browserProps.actionTargets)
+    this.props.browserProps.deleteFile([this.props]);
+  }
+
+  handleViewSubmit = (event) => {
+    event.preventDefault()
+    console.log("handleViewSubmit:", event, this.props);
+    if (!this.props.browserProps.viewFile) {
+      console.log("handleViewSubmit: viewFile not set");
+      return
+    }
+
+    this.props.browserProps.viewFile([this.props]);
+  }
+
+  handleRefreshSubmit = (event) => {
+    event.preventDefault()
+    console.log("handleRefreshSubmit:", event, this.props);
+    if (!this.props.browserProps.refreshFile) {
+      console.log("handleRefreshSubmit: refreshFile not set");
+      return
+    }
+
+    this.props.browserProps.refreshFile([this.props]);
   }
 
   handleCancelEdit = (event) => {
@@ -152,9 +206,10 @@ class BaseFile extends React.Component {
       render = this.props.connectDragSource(render)
     }
     if (
-      typeof this.props.browserProps.createFiles === 'function' ||
-      typeof this.props.browserProps.moveFile === 'function' ||
-      typeof this.props.browserProps.moveFolder === 'function'
+      (typeof this.props.browserProps.createFiles === 'function' ||
+       typeof this.props.browserProps.moveFile === 'function' ||
+       typeof this.props.browserProps.moveFolder === 'function') &&
+      (!(this.props.isStudent))
     ) {
       render = this.props.connectDropTarget(render)
     }
@@ -162,20 +217,46 @@ class BaseFile extends React.Component {
   }
 }
 
+// Folders are selected as string, files are selected as objects
+const baseFileIsItemSelected = (item) => {
+  let foundItem = false;
+  let selection = item.browserProps.selection;
+  console.log("baseFileIsItemSelected:", item);
+  for (let idx in selection) {
+    let curSel = selection[idx];
+    if (typeof curSel === "string") {
+      console.log("baseFileIsItemSelected: string match")
+      foundItem = (curSel === item.key) ? true : false;
+      if (foundItem) break;
+    } else if (curSel.id === item.id) {
+      console.log("baseFileIsItemSelected: id match")
+      foundItem = true;
+      break;
+    }
+  }
+
+  return foundItem;
+}
+
 const dragSource = {
   beginDrag(props) {
+    // Cannot use a simple includes any more as folders are
+    // strings but files are now objects. To handle this, we use
+    // the new function "baseFileIsItemSelected"
     if (
       !props.browserProps.selection.length ||
-      !props.browserProps.selection.includes(props.fileKey)
+      !baseFileIsItemSelected(props)
     ) {
-      props.browserProps.select(props.fileKey, 'file')
+      console.log("beginDrag:", props);
+      props.browserProps.select(props, 'file')
     }
     return {
-      key: props.fileKey,
+      key: props,
     }
   },
 
   endDrag(props, monitor, component) {
+    console.log("endDrag:", props, monitor, component);
     moveFilesAndFolders(props, monitor, component)
   },
 }
